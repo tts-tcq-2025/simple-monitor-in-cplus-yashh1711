efb46c69b14ba98c63029264b91cc5d07ec7ecd5
Yash Nagar <nagar.yash107@gmail.com>
2025-07-21 11:51:28 +0530

    introduced check flags

--- Checker.cpp
+++ Checker.cpp
@@ -6,35 +6,44 @@ bool IsTempOutOfRange(float temperature)
 {
   if (temperature < 0 || temperature > 45)
     return true;
+  return false;
 }
 
 bool IsSocOutOfRange(float soc)
 {
   if (soc < 20 || soc > 80)
     return true;
+  return false;
 }
 
 bool IsChargeRateOutOfRange(float chargeRate)
 {
   if (chargeRate > 0.8)
     return true;
+  return false;
 }
 
 bool batteryIsOk(float temperature, float soc, float chargeRate)
 {
+  bool tempOut, socOut, chargeRateOut;
+  tempOut = socOut = chargeRateOut = false;
   if (IsTempOutOfRange(temperature))
   {
     cout << "Temperature out of range!\n";
-    return false;
+    tempOut = true;
   }
   if (IsSocOutOfRange(soc))
   {
     cout << "State of Charge out of range!\n";
-    return false;
+    socOut = true;
   }
   if (IsChargeRateOutOfRange(chargeRate))
   {
     cout << "Charge Rate out of range!\n";
+    chargeRateOut = true;
+  }
+  if (tempOut || socOut || chargeRateOut)
+  {
     return false;
   }
   return true;
