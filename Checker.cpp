482efa3ff56a7a60738a37292a17a96b0e2187b6
Yash Nagar <nagar.yash107@gmail.com>
2025-07-21 12:06:57 +0530

    updated singular functions with io

--- Checker.cpp
+++ Checker.cpp
@@ -2,51 +2,43 @@
 #include <iostream>
 using namespace std;
 
-bool IsTempOutOfRange(float temperature)
+bool isTempOutOfRange(float temperature)
 {
   if (temperature < 0 || temperature > 45)
+  {
+    cout << "Temperature out of range!\n";
     return true;
+  }
   return false;
 }
 
-bool IsSocOutOfRange(float soc)
+bool isSocOutOfRange(float soc)
 {
   if (soc < 20 || soc > 80)
+  {
+    cout << "State of Charge out of range!\n";
     return true;
+  }
   return false;
 }
 
-bool IsChargeRateOutOfRange(float chargeRate)
+bool isChargeRateOutOfRange(float chargeRate)
 {
   if (chargeRate > 0.8)
+  {
+    cout << "Charge Rate out of range!\n";
     return true;
+  }
   return false;
 }
 
 bool batteryIsOk(float temperature, float soc, float chargeRate)
 {
-  bool tempOut, socOut, chargeRateOut;
-  tempOut = socOut = chargeRateOut = false;
-  if (IsTempOutOfRange(temperature))
+  if (!(isTempOutOfRange(temperature)) && !(isSocOutOfRange(soc)) && !(isChargeRateOutOfRange(chargeRate)))
   {
-    cout << "Temperature out of range!\n";
-    tempOut = true;
+    return true;
   }
-  if (IsSocOutOfRange(soc))
-  {
-    cout << "State of Charge out of range!\n";
-    socOut = true;
-  }
-  if (IsChargeRateOutOfRange(chargeRate))
-  {
-    cout << "Charge Rate out of range!\n";
-    chargeRateOut = true;
-  }
-  if (tempOut || socOut || chargeRateOut)
-  {
-    return false;
-  }
-  return true;
+  return false;
 }
 
 int main()
